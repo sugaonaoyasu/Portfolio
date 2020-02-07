2d2d6fa2d961734a19cf7bbcfe6fbd301385d4a2
菅生直裕 <sugaonaoyasu@sugounaohironoMacBook-Pro.local>
2020-02-07 12:54:01 +0900

    HTML,CSS,スライドショー追加

---
+++ main.js
@@ -0,0 +1,17 @@
+// let imgItems = document.querySelectorAll('.img-items');
+// cnt = 0;
+// console.log(imgItems);
+
+// imgItems[0].classList.add("active");
+
+// let slideShow = function() {
+// if (cnt <= 2) {
+//     imgItems[cnt].classList.remove("active");
+//     cnt++;
+// } else if(cnt === 3) {
+//     imgItems[3].classList.remove("active");
+//     cnt = 0;
+// }
+//     imgItems[cnt].classList.add("active");
+// }
+// setInterval(slideShow,2000)
